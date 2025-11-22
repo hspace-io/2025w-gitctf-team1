25e0a388221e413649cc79b0956ed9034073828a
MyNameSarah <yd2758@gmail.com>
2025-11-22 17:16:06 +0900

    BE event 기능구현 및 Foregin key 오류 수정

--- db.js
+++ db.js
@@ -2,12 +2,21 @@ const Database = require('better-sqlite3');
 const path = require('path');
 
 // DB 파일 경로설정 data 폴더가 프로젝트 루트에 있어야 함
-const dbPath = path.join(__dirname, 'data', 'frontierCTF.db');
+const DATABASE_FILE = path.join(path.resolve(), 'data', 'frontierCTF.db');
 
-const db = new Database(dbPath, {
-  verbose: console.log // 디버깅용 쿼리 실행 로그 출력
-});
+let dbInstance;
 
-console.log("SQLite DB Connected!");
+try {
+    dbInstance = new Database(DATABASE_FILE, {
+        verbose: console.log  // 디버깅용 쿼리 실행 로그 출력
+    });
+    
+    dbInstance.exec('PRAGMA foreign_keys = ON;');
+    
+    console.log("INFO: SQLite DB (better-sqlite3) connected successfully.");
+} catch (error) {
+    console.error(`FATAL ERROR: Database connection failed. Path: ${DATABASE_FILE}`, error);
+    dbInstance = null;
+}
 
-module.exports = db;
+module.exports = dbInstance;
